6b2f8e95e35898deceb037630a4335fe3a2f6442
EC2 Default User <ec2-user@ip-172-31-4-79.ap-southeast-2.compute.internal>
2025-08-24 23:52:55 +0000

    [Update] bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,27 @@ class BooksController < ApplicationController
   # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
+    
 
     @book.user_id = current_user.id
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    @book.save
     # 4. トップ画面へリダイレクト
     redirect_to books_path
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+    @book_show = Book.find(params[:id])
   end
 
   def edit
